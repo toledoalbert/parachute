a755155a1fbd1b9067ba666aa479f4f6f734ea7e
Albert Toledo <toledoalbert@gmail.com>
2013-10-03 15:14:33 -0400

    landing page done

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Parachute | Land on your feet when relocated!</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <link href="css/pchute.css" rel="stylesheet" media="screen">
+</head>
+<body>
+  <div class="container">
+    <div class="text-center brand"> 
+      <h1>Parachute</h1>
+      <div class="row">
+        <div class="span12">
+          <form method="get" action="/" class="form-inline" >
+            <input name="q" class="span5 mainIn" type="text"  placeholder="$ Enter Salary" >
+            <input name="loc" class="span5 mainIn" type="text"  placeholder="Enter Zip Code">
+            <button type="submit" class="btn btn-cstm mainIn" value="Go!"> <i class="icon-search icon-white"></i> Go!</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="desc">
+      <p>
+        Parachute is a tool that lets you search for real estates that fits
+        your salary and expectations when moving from one place to another
+        or when you have a job relocation.
+      </p>
+
+    </div>
+
+  </div>
+  <script src="http://code.jquery.com/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
