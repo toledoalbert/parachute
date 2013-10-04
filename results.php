a46e93aadd19ec58638e1492fa60d151119bafcd
Albert Toledo <toledoalbert@gmail.com>
2013-10-03 22:08:39 -0400

    results page design improvements for each result display

--- results.php
+++ results.php
@@ -11,57 +11,135 @@
 <body>
 
   <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="topbar">
-        <div class="container">
-          <button type="button" class="btn btn-navbar tgl" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="parachute brand" href="./index.php"><b>Para</b>chute</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav pull-right">
-              <form class="navbar-form form-inline pull-right" method="get" action="results.php">
-    <input type="text" placeholder="Enter Salary" name="salary" class="inputField">
-    <input type="password" placeholder="Enter Zip Code" name="zip" class="inputField">
+    <div class="topbar">
+      <div class="container">
+        <button type="button" class="btn btn-navbar tgl" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="parachute brand" href="./index.php"><b>Para</b>chute</a>
+        <div class="nav-collapse collapse">
+          <ul class="nav pull-right">
+            <form class="navbar-form form-inline pull-right" method="get" action="results.php">
+              <input type="text" placeholder="Enter Salary" name="salary" class="inputField">
+              <input type="password" placeholder="Enter Zip Code" name="zip" class="inputField">
 
-    <button type="submit" class="btn btn-go"><i class="icon-search icon-white"></i>Go!</button>
-</form>
-            </ul>
-          </div>
+              <button type="submit" class="btn btn-go"><i class="icon-search icon-white"></i>Go!</button>
+            </form>
+          </ul>
         </div>
       </div>
     </div>
+  </div>
 
   <div class="container results">
 
     <div class="row text-center">
-      <div class="span4">
+      <div class="span4 neigh">
         <h3>Neighborhoods</h3>
-        <div class="sect">Section for Info</div>
-        <div class="sect">Section for Info</div>
-        <div class="sect">Section for Info</div>
+        <div class="sect">Neighborhood</div>
+        <div class="sect">Neighborhood</div>
+        <div class="sect">Neighborhood</div>
+        <div class="sect">Neighborhood</div>
+        <div class="sect">Neighborhood</div>
+        <div class="sect">Neighborhood</div>
+        <div class="sect">Neighborhood</div>
       </div>
-      <div class="span4">
+      <div class="span4 home">
         <h3>Homes</h3>
-        <div class="sect">Section for Info</div>
-        <div class="sect">Section for Info</div>
-        <div class="sect">Section for Info</div>
-      </div>
-      <div class="span4">
-        <h3>Rentals</h3>
-        <div class="sect">Section for Info</div>
-        <div class="sect">Section for Info</div>
-        <div class="sect">Section for Info</div>
+        <div class="sect">
+          <div class="row text-left">
+            <div class="span3">
+              <img src="img/background.jpg" alt="">
+            </div>
+            <div class="span9">
+              <div class="name">My future house</div>
+              <div class="address">124A Mariners Cove Hampton VA</div>
+              <div class="price">$100,000</div>
+            </div>
+          </div>
+        </div>
+        <div class="sect">
+          <div class="row text-left">
+            <div class="span3">
+              <img src="img/background.jpg" alt="">
+            </div>
+            <div class="span9">
+              <div class="name">My future house</div>
+              <div class="address">124A Mariners Cove Hampton VA</div>
+              <div class="price">$100,000</div>
+            </div>
+          </div>
+        </div><div class="sect">
+        <div class="row text-left">
+          <div class="span3">
+            <img src="img/background.jpg" alt="">
+          </div>
+          <div class="span9">
+            <div class="name">My future house</div>
+            <div class="address">124A Mariners Cove Hampton VA</div>
+            <div class="price">$100,000</div>
+          </div>
+        </div>
+      </div><div class="sect">
+      <div class="row text-left">
+        <div class="span3">
+          <img src="img/background.jpg" alt="">
+        </div>
+        <div class="span9">
+          <div class="name">My future house</div>
+          <div class="address">124A Mariners Cove Hampton VA</div>
+          <div class="price">$100,000</div>
+        </div>
       </div>
     </div>
-    
-
-
   </div>
+  <div class="span4 rent">
+    <h3>Rentals</h3>
+    <div class="sect">
+          <div class="row text-left">
+            <div class="span3">
+              <img src="img/background.jpg" alt="">
+            </div>
+            <div class="span9">
+              <div class="name">My future house</div>
+              <div class="address">124A Mariners Cove Hampton VA</div>
+              <div class="price">$100,000</div>
+            </div>
+          </div>
+        </div><div class="sect">
+          <div class="row text-left">
+            <div class="span3">
+              <img src="img/background.jpg" alt="">
+            </div>
+            <div class="span9">
+              <div class="name">My future house</div>
+              <div class="address">124A Mariners Cove Hampton VA</div>
+              <div class="price">$100,000</div>
+            </div>
+          </div>
+        </div><div class="sect">
+          <div class="row text-left">
+            <div class="span3">
+              <img src="img/background.jpg" alt="">
+            </div>
+            <div class="span9">
+              <div class="name">My future house</div>
+              <div class="address">124A Mariners Cove Hampton VA</div>
+              <div class="price">$100,000</div>
+            </div>
+          </div>
+        </div>
+  </div>
+</div>
 
-  <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+
+
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 
 </body>
 </html>
